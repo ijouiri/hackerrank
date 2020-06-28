9306bcf15c5898034221a5e7472dc42799d1e7cf
abdellah ijouiri <57574897+ijouiri@users.noreply.github.com>
2020-06-28 20:32:53 +0100

    Add files via upload

---
+++ alphabetrangoli.py
@@ -0,0 +1,22 @@
+l=list()
+newlist=list()
+
+if __name__=="__main__":
+    n=int(input())
+    k=96+n
+    for i in range (n):
+        l.append(chr(k))
+        k-=1
+    l.sort()
+    for i in range(n):
+        s="-".join(l[i:n])
+        newlist.append((s[::-1]+s[1:]).center(4*n-3,'-'))
+
+    print ("\n".join(newlist[::-1]+newlist[1:]))
+
+    # -- or --
+   # for i in reversed(newlist):
+   #     print (i)
+   # for i in newlist[1:]:
+   #     print (i)
+
